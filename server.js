2d7f8ebbd0c0785f9ac54fea2dc9e753d1b8c4af
Matias Racedo <matiasracedo@gmail.com>
2025-04-22 19:11:09 -0300

    adding endpoint for self hosted instance

--- server.js
+++ server.js
@@ -17,13 +17,31 @@ app.post('/action/uniqueSession', async (req, res) => {
   }
 
   try {
-    await uniqueSession(userID);
+    await uniqueSession(userID, 'cloud');
     res.status(200).json({ status: 'Session cleanup complete' });
   } catch (err) {
     console.error('Failed to process uniqueSession:', err);
     res.status(500).json({ error: 'Internal server error' });
   }
 }); 
+
+app.post('/local/uniqueSession', async (req, res) => {
+
+    const { userID } = req.body;
+  
+    if (!userID) {
+      console.error('Missing userID in payload');
+      return res.status(400).json({ error: 'Missing userID in payload' });
+    }
+  
+    try {
+      await uniqueSession(userID, 'local');
+      res.status(200).json({ status: 'Session cleanup complete' });
+    } catch (err) {
+      console.error('Failed to process uniqueSession:', err);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  }); 
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
