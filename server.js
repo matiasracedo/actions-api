f15376ced56c34b4581dae1ec1d9b68bdbe9658f
Matias Racedo <matiasracedo@gmail.com>
2025-05-06 15:12:32 -0300

    added test endpoint for custom claims

--- server.js
+++ server.js
@@ -27,8 +27,16 @@ app.post('/action/uniqueSession', async (req, res) => {
 
 app.post('/action/test', async (req, res) => {
 
+    let claims = {
+        append_claims: [
+          {
+            key: "custom_claim",
+            value: "Added from Action v2"
+          }
+        ]
+      };
     console.log('Received request:', req.body);
-    res.status(200).json({ status: 'All good...' });
+    res.status(200).json(claims);
   }); 
 
 app.listen(PORT, () => {
